0cf37ba458c8cbd4e7fe048c2f8949c00212cc42
Shanza Khalid <shanzakhalid861@gmail.com>
2026-02-05 02:53:51 -0800

    Update Dashboard-final.py

--- Dashboard-final.py
+++ Dashboard-final.py
@@ -338,8 +338,6 @@ elif page == "Interactive map":
     with open(map_found, "r", encoding="utf-8") as f:
         html_data = f.read()
 
-   st.markdown("### 🗺️ Trip Flow Map (Top Routes)")
-
 # Optional: short context line that looks professional
 st.caption("Thicker/brighter routes represent higher trip volume between station pairs.")
 
